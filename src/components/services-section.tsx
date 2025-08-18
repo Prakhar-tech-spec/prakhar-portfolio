9eb039f6b58ff0c5d4e1f8a8520fcb4ca94135c9
Prakhar Creatific <prakharr.creatific@gmail.com>
2025-08-18 06:48:51 +0000

    Also in laptop when hovered the card should turn green with service name

--- src/components/services-section.tsx
+++ src/components/services-section.tsx
@@ -43,12 +43,12 @@ export function ServicesSection() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
           {services.map((service) => (
-            <Card key={service.title} className="group text-center flex flex-col items-center p-6 bg-background border-2 border-transparent hover:border-primary transition-all duration-300 transform hover:-translate-y-2 shadow-lg">
+            <Card key={service.title} className="group text-center flex flex-col items-center p-6 bg-background border-2 border-transparent md:hover:bg-primary transition-all duration-300 transform md:hover:-translate-y-2 shadow-lg">
               <CardHeader className="p-0 pt-6">
-                <CardTitle className="mt-4 font-headline">{service.title}</CardTitle>
+                <CardTitle className="mt-4 font-headline md:group-hover:font-cursive md:group-hover:text-primary-foreground">{service.title}</CardTitle>
               </CardHeader>
               <CardContent className="flex-1 flex items-center transition-all duration-300">
-                  <p className="text-muted-foreground md:max-h-0 md:opacity-0 group-hover:max-h-40 group-hover:opacity-100 transition-all duration-500 ease-in-out">
+                  <p className="text-muted-foreground md:max-h-0 md:opacity-0 group-hover:max-h-40 group-hover:opacity-100 transition-all duration-500 ease-in-out md:group-hover:text-primary-foreground">
                     {service.description}
                   </p>
               </CardContent>
